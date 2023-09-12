6437c9b5c006a43ce60e8ec47a554735ebed9e69
DerylFeyza <muhammadderylfeyza@gmail.com>
2023-09-12 13:50:11 +0700

    eep

--- auction.php
+++ auction.php
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="mystyle.css">
+<link rel="stylesheet" href="css/mystyle.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-<?php include "footer.php" ?>
+
 </body>
 </html>
