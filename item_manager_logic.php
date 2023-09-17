cf58d486d20d4a58a68df57cb5b02be6f9fd11d2
DerylFeyza <muhammadderylfeyza@gmail.com>
2023-09-17 17:00:13 +0700

    item manage

---
+++ item_manager_logic.php
@@ -0,0 +1,40 @@
+<?php
+include "koneksi.php";
+if(isset($_POST['auctioned_button'])){
+    $statusFilter = 'Auctioned';
+    displayItems($statusFilter);
+} 
+elseif(isset($_POST['pending_button'])){
+    $statusFilter = 'Pending';
+    displayItems($statusFilter);
+}
+elseif(isset($_POST['approved_button'])){
+    $statusFilter = 'approved';
+    displayItems($statusFilter);
+}
+
+function displayItems($statusFilter) {
+    include "koneksi.php";
+    $statusFilter = mysqli_real_escape_string($conn, $statusFilter); 
+    $qry_item = mysqli_query($conn, "SELECT * FROM item WHERE status LIKE '$statusFilter'");
+    while ($dt_item = mysqli_fetch_array($qry_item)) {
+        ?>
+        <div class="card mb-3" style="max-width: 500px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="/Project_PHP/itemasset/<?= $dt_item['cover'] ?>" class="card-img-top">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $dt_item['name'] ?></h5>
+                        <h4 class="card-title"><?= $dt_item['startprice'] ?></h4>
+                        <p class="card-text"><?= substr($dt_item['deskripsi'], 0, 20) ?></p>
+                        <a href="hapus_item.php?id=<?=$dt_item['id']?>" onclick="return confirm('Apakah anda yakin menghapus data ini?')" class="btn btn-danger">Hapus</a></td>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+}
+?>
