68c86ceba00268551c71ac20f5d9b2cf294a21bb
DerylFeyza <muhammadderylfeyza@gmail.com>
2023-10-25 09:18:22 +0700

    fix: signup fix

--- signup.php
+++ signup.php
@@ -1,18 +1,15 @@
 <?php include
     "header.php"
     ?>
+<link rel="stylesheet" href="css/loginstyle.css">
 <div class="sign-up-container">
     <div class="form-container signup-container">
         <form class="login" action="proses_signup.php" method="post">
-            <h1>Sign up</h1>
-            <div class="social-container">
-                <a href="#" class="social"><i class="fa fa-facebook fa-2x"></i></a>
-                <a href="#" class="social"><i class="fab fa fa-twitter fa-2x"></i></a>
-            </div>
+            <h1 >Sign up</h1>
             <input autocomplete="off" type="text" name="username" value="" class="form-control" />
             <input type="password" name="password" class="form-control" />
+            <button style="margin-top: 50px;" type="submit" id="Login">Sign Up</button>
             <a href="login.php">Back to login page</a>
-            <button type="submit" id="Login">Sign Up</button>
         </form>
     </div>
     </body>
