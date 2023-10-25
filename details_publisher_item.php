29ca3e76e931775fdd581ae22e7668b316cd6f22
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-10-26 06:56:10 +0700

    [style] restyle publisher

--- details_publisher_item.php
+++ details_publisher_item.php
@@ -8,58 +8,51 @@ if (isset($_GET['id_item'])) {
 } else if (!isset($_GET['id_item'])) {
     'location: auction.php';
 }
-$item_id=$_SESSION['item_id'];
-$qry_detail_item = mysqli_query($conn, "select * from item where id = '" .$item_id . "'");
+$item_id = $_SESSION['item_id'];
+$qry_detail_item = mysqli_query($conn, "select * from item where id = '" . $item_id . "'");
 $dt_item = mysqli_fetch_array($qry_detail_item);
 ?>
-<div class="container bid-container">
-    <div class="card-wrapper center">
-            <div class="row">
-                <div class="image-container">
-                    <img src="/Project_PHP/itemasset/<?= $dt_item['cover'] ?>" method="post">
-                </div>
-                <div class="item-info">
-                    <thead>
-                        <tr>
-                            <td>
-                                <h1>
-                                    <?= $dt_item['name'] ?>
-                                </h1>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p></p>
-                                <?= $dt_item['deskripsi'] ?>
-                                </p>
-                            </td>
-                            <td>
-                                <p></p>Start Price:
-                                <?= $dt_item['startprice'] ?>
-                                </p>
-                            </td>
-                        </tr>
-                    </thead>
+<div class="container">
+    <div class="row justify-content-evenly">
+        <div class="col-md-4">
+            <div class="card shadow">
+                <img src="/Project_PHP/itemasset/<?= $dt_item['cover'] ?>" method="post" class="card-img-top">
+                <div class="card-body">
+                    <h2 class="card-title">
+                        <?= $dt_item['name'] ?>
+                    </h2>
+                    <h5 class="card-title">
+                        <?= $dt_item['startprice'] ?>
+                    </h5>
+                    <p class="card-text">
+                        <?= substr($dt_item['deskripsi'], 0, 20) ?>
+                    </p>
                 </div>
             </div>
+        </div>
+
+        <div class="col-md-7">
+            <div id="response"></div>
+            <div class="container mt-5">
+                <form id="myForm" enctype="multipart/form-data" method="post" action="proses_update_details.php">
+                    <input type="hidden" name="id" value="<?= $dt_item['id'] ?>">
+                    nama:
+                    <input autocomplete="off" type="text" name="name" value="<?= $dt_item['name'] ?>"
+                        class="form-control" required>
+                    harga awal:
+                    <input autocomplete="off" type="integer" name="startprice" value="<?= $dt_item['startprice'] ?>"
+                        class="form-control" required>
+                    Deskripsi:
+                    <input autocomplete="off" type="text" name="deskripsi" value="<?= $dt_item['deskripsi'] ?>"
+                        class="form-control" required>
+                    Foto:
+                    <input autocomplete="off" type="file" name="foto" class="form-control" required>
+                    <input type="submit" value="Tambah Buku" class="btn btn-primary">
+                </form>
+            </div>
+        </div>
     </div>
-</div>
 
-<div class="container" id="tambah-item-form" >
-    <form id="myForm" enctype="multipart/form-data" method="post" action="proses_update_details.php">
-    <input type="hidden" name="id" value="<?= $dt_item['id'] ?>">
-        nama:
-        <input autocomplete="off" type="text" name="name" value="<?= $dt_item['name'] ?>" class="form-control" required>
-        harga awal:
-        <input autocomplete="off" type="integer" name="startprice" value="<?= $dt_item['startprice'] ?>" class="form-control" required>
-        Deskripsi:
-        <input autocomplete="off" type="text" name="deskripsi" value="<?= $dt_item['deskripsi'] ?>" class="form-control" required>
-        Foto:
-        <input autocomplete="off" type="file" name="foto" class="form-control" required>
-        <input type="submit" value="Tambah Buku" class="btn btn-primary">
-    </form>
-
-    <div id="response"></div>
 </div>
 </body>
 
--- publisher_item.php
+++ publisher_item.php
@@ -6,25 +6,25 @@ if (!isset($_SESSION['id'])) {
 }
 ?>
 <div class="container" style="padding-top: 4rem;">
-  <div class="row">
+  <div class="row gap-3">
     <?php
     $qry_item = mysqli_query($conn, "select * from item where id_publisher = '" . $_SESSION['id'] . "'");
     while ($dt_item = mysqli_fetch_array($qry_item)) {
       ?>
-      <div class="smh col-md-3">
-        <div class="card">
+      <div class="col-md-3">
+        <div class="card shadow">
           <img src="/Project_PHP/itemasset/<?= $dt_item['cover'] ?>" class="card-img-top">
           <div class="card-body">
-            <h5 class="card-title">
+            <h2 class="card-title">
               <?= $dt_item['name'] ?>
-            </h5>
-            <h4 class="card-title">
+            </h2>
+            <h5 class="card-title">
               <?= $dt_item['startprice'] ?>
-            </h4>
+            </h5>
             <p class="card-text">
               <?= substr($dt_item['deskripsi'], 0, 20) ?>
             </p>
-            <a href="details_publisher_item.php?id_item=<?= $dt_item['id'] ?>" class="btn">See Details</a>
+            <a href="details_publisher_item.php?id_item=<?= $dt_item['id'] ?>" class="btn btn-primary w-100">See Details</a>
           </div>
         </div>
       </div>
