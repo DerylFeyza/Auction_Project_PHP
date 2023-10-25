5758fd39faf9d13596e69847755c1a8975352368
DerylFeyza <muhammadderylfeyza@gmail.com>
2023-10-25 09:00:51 +0700

    fix: item manager design

--- item_manager.php
+++ item_manager.php
@@ -2,14 +2,15 @@
 include "header.php";
 ?>
 <div class="container" style="padding-top: 5rem;">
-    <form method="post" class="d-flex justify-content-evenly">
-        <button type="submit" name="auctioned_button" class="btn btn-primary shadow">Show Auctioned Items</button>
-        <button type="submit" name="pending_button"  class="btn btn-primary shadow">Show Pending Items</button>
-        <button type="submit" name="approved_button"  class="btn btn-primary shadow">Show Approved Items</button>
-        <button type="submit" name="sold_button"  class="btn btn-primary shadow">Show sold Items</button>
-        <button type="submit" name="cancelled_button"  class="btn btn-primary shadow">Show cancelled Items</button>
-        <?php include "item_manager_logic.php"; ?>
-    </form>
+    <div class="section">
+        <form method="post" class="row justify-content-evenly">
+            <button type="submit" name="auctioned_button" class="col-2 btn btn-primary shadow">Show Auctioned Items</button>
+            <button type="submit" name="pending_button" class="col-2  btn btn-primary shadow">Show Pending Items</button>
+            <button type="submit" name="approved_button" class="col-2  btn btn-primary shadow">Show Approved Items</button>
+            <button type="submit" name="sold_button" class="col-2  btn btn-primary shadow">Show sold Items</button>
+            <button type="submit" name="cancelled_button" class="col-2  btn btn-primary shadow">Show cancelled Items</button>
+            <?php include "item_manager_logic.php"; ?>
+        </form>
+    </div>
 </div>
 <?php include "footer.php" ?>
-
