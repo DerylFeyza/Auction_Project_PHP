9e9cfe369349c13734b21e1cc4fd79513b204a83
DerylFeyza <muhammadderylfeyza@gmail.com>
2023-10-28 11:40:19 +0700

    updated db

--- item_update_status.php
+++ item_update_status.php
@@ -8,9 +8,6 @@ if (isset($_GET['id']) && isset($_GET['action'])) {
     switch ($action) {
         case 'Reject':
             $result = mysqli_query($conn, "select cover from item where id='" . $_GET['id'] . "'");
-            $row = mysqli_fetch_assoc($result);
-            $filename = $row['cover'];
-            unlink('itemasset/' . $filename);
             $qry_hapus = mysqli_query($conn, "delete from item where id='" . $_GET['id'] . "'");
             $qry_hapusbid = mysqli_query($conn, "delete from bids where id_item='" . $_GET['id'] . "'");
             if ($qry_hapus) {
--- proses_update_details.php
+++ proses_update_details.php
@@ -5,34 +5,6 @@ if($_POST){
     $deskripsi = $_POST['deskripsi'];
     $startprice = $_POST['startprice'];
     include "koneksi.php";
-
-    $target_dir = "itemasset/";
-    $target_file = $target_dir . basename($_FILES["foto"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-    // Check if image file is a valid image
-    if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["foto"]["tmp_name"]);
-        if ($check !== false) {
-            $uploadOk = 1;
-        } else {
-            echo "<script>alert('Gagal update');location.href='proses_update_details.php?id_item=" . $id_item . "';</script>";
-        }
-    }
-
-    // Check file size
-    if ($_FILES["foto"]["size"] > 500000) {
-        echo "<script>alert('Gagal update file kebesaran');location.href='proses_update_details.php?id_item=" . $id_item . "';</script>";
-    }
-
-    // Allow only specific file formats
-    if (
-        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif"
-    ) {
-        echo "<script>alert('Gagal update');location.href='proses_update_details.php?id_item=" . $id_item . "';</script>";
-    }
     
     $update = mysqli_query($conn, "update item set name='" . $name . "', cover='" . $_FILES["foto"]["name"] . ", cover='" . $deskripsi . "', startprice='" . $startprice . "' where id = '".$id."' ") or die(mysqli_error($conn));
     if ($update) {
