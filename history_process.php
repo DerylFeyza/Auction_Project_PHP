dd42394c5d9d3de15fc68be505c42b301af8858c
DerylFeyza <muhammadderylfeyza@gmail.com>
2023-10-31 14:24:58 +0700

    add: history feature

---
+++ history_process.php
@@ -0,0 +1,28 @@
+<?php 
+    include "koneksi.php";
+    
+
+if ($_POST) {
+    $id = $_POST['id'];
+    $history_query = mysqli_query($conn, "SELECT * FROM bids WHERE bid = (SELECT MAX(bid) FROM bids) AND id_item = ".$id); 
+    $dt_history = mysqli_fetch_array($history_query);
+    $id_item = $dt_history["id_item"];
+    $id_winner = $dt_history["id_user"];
+    $final_bid = $dt_history["bid"];
+
+    $winner_query =   mysqli_query($conn, "SELECT username FROM client WHERE id =".$id_winner); 
+    $dt_history_winner = mysqli_fetch_array($winner_query);
+    $winner = $dt_history_winner["username"];
+
+    $insert = mysqli_query($conn, "insert into history (id_item, id_winner, winner, final_bid) 
+    value ('" . $id_item . "','" . $id_winner . "', '" . $winner . "', '".$final_bid."')") or die(mysqli_error($conn));
+
+    if ($insert) {
+        echo "<script>alert('Sukses tambah history');location.href='item_manager.php';</script>";
+    }
+    else{
+        echo "<script>alert('Sukses tambah history');location.href='item_manager.php';</script>";
+
+    }
+}
+?>
